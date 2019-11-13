22e8e14738e794bfa4d95a5eff8e41d7a416435f
corefarm <thesky_x@yahoo.com>
2019-11-14 00:56:21 +0700

    add koa protect

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const path = require('path')
 const koaBody = require('koa-body')
 const cors = require('@koa/cors')
 const session = require('koa-session')
+const protect = require('koa-protect')  
+const ratelimit = require('koa-ratelimit')
+const redis = require('redis')
+const client = redis.createClient()  
 const app = new Koa 
 
 render(app, {
@@ -13,12 +17,35 @@ render(app, {
     cache: false
   });
 
-app.keys = ['Love Job, its a secret!'];
-app.use(session(app)); 
-app.use(cors())
+app.keys = ['Love Job, its a secret!']
+app.use(session(app))
+app.use(cors()) 
 
+app.use(protect.koa.sqlInjection({  
+  body: true,  
+  loggerFunction: console.error  
+}))  
+
+app.use(protect.koa.xss({  
+  body: true,  
+  loggerFunction: console.error  
+}))  
+
+app.use(ratelimit({  
+  db: client,  
+  duration: 60000,  
+  errorMessage: 'Sometimes You Just Have to Slow Down.',  
+  id: (ctx) => ctx.ip,  
+  headers: {  
+      remaining: 'Rate-Limit-Remaining',  
+      reset: 'Rate-Limit-Reset',  
+      total: 'Rate-Limit-Total'  
+  },  
+  max: 100  
+}))
 app.use(koaBody({
 	multipart: true
 }))
 app.use(require('./src/route/')) 
 app.listen(3000)
+
